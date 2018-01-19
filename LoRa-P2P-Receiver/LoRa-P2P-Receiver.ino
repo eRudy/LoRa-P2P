79e4fcb136530b71d4a3727f2917cbf8f47bf15f
eRudy <herge11@free.fr>
2018-01-19 22:50:26 +0100

    Small cleaning

--- LoRa-P2P-Receiver/LoRa-P2P-Receiver.ino
+++ LoRa-P2P-Receiver/LoRa-P2P-Receiver.ino
@@ -4,63 +4,87 @@
 #define FASTLED_INTERNAL // disable PRAGMA warnings
 #include <FastLED.h>
 
+#define BAUD_RATE 9600
+
+// we can solder leds on this cool PCB :) (https://github.com/hallard/WeMos-Lora)
 #define LED_TYPE      WS2812B
 #define COLOR_ORDER   GRB
 #define NUM_LEDS      2
 #define DATA_PIN      0
 
+// and a switch button
+#define BUTTON_PIN 2
+
+const short led_duration = 1000;
+
+
+unsigned long previousMillis_led = 0;	// will store last time led was turned on
+
+int counter = 0;			// messages counter
+
 CRGB leds[NUM_LEDS];
 
-const long interval_led = 1000;
-unsigned long previousMillis_led = 0;        // will store last time led was turned on
+
+
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+	Serial.begin(BAUD_RATE);
+	while (!Serial);
 
-FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+
+	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+
+
+
 
 #ifdef ESP8266
-  LoRa.setPins(16, 17, 15); // set CS, reset, IRQ pin
+	LoRa.setPins(16, 17, 15); // set CS, reset, IRQ pin
 #endif
 
-  Serial.println("LoRa Receiver");
-
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+	Serial.println("LoRa Receiver");
+	if (!LoRa.begin(868E6)) {
+		Serial.println("Starting LoRa failed!");
+		while (1);
+	}
 }
+
+
+
+
+
 
 void loop() {
 
-unsigned long currentMillis = millis();
-  
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
+	unsigned long currentMillis = millis();
 
-    previousMillis_led = currentMillis;
-    leds[0] = CRGB::Green;
-    FastLED.show();
+	// try to parse packet
+	int packetSize = LoRa.parsePacket();
+	if (packetSize) {
+		// received a packet
+		Serial.print("Received packet '");
 
-    
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
+		leds[0] = CRGB::Green;
+		FastLED.show();
 
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-  }
+		// save the last time the led was turned on
+		previousMillis_led = currentMillis;
 
-  // turn off after delay
-if (currentMillis - previousMillis_led >= interval_led) {
-  leds[0] = CRGB::Black;
-  FastLED.show();
-}
+		// read packet
+		while (LoRa.available()) {
+			Serial.print((char)LoRa.read());
+		}
+
+		// print RSSI of packet
+		Serial.print("' with RSSI ");
+		Serial.println(LoRa.packetRssi());
+
+		counter++;
+	}
+
+	// turn off led after desired duration
+	if (currentMillis - previousMillis_led >= led_duration) {
+		leds[0] = CRGB::Black;
+		FastLED.show();
+	}
 
 }
--- LoRa-P2P-Sender/LoRa-P2P-Sender.ino
+++ LoRa-P2P-Sender/LoRa-P2P-Sender.ino
@@ -4,83 +4,85 @@
 #define FASTLED_INTERNAL // disable PRAGMA warnings
 #include <FastLED.h>
 
+#define BAUD_RATE 9600
+
+// we can solder leds on this cool PCB :) (https://github.com/hallard/WeMos-Lora)
 #define LED_TYPE      WS2812B
 #define COLOR_ORDER   GRB
 #define NUM_LEDS      2
 #define DATA_PIN      0
 
+// and a switch button
 #define BUTTON_PIN 2
 
-int counter = 0;
+const short led_duration = 1000;
+
+unsigned long previousMillis = 0;	// will store last time message was sent
+unsigned long previousMillis_led = 0;	// will store last time led was turned on
+
+int counter = 0;			// messages counter
 
 CRGB leds[NUM_LEDS];
 
-unsigned long previousMillis = 0;        // will store last time message was sent
-unsigned long previousMillis_led = 0;        // will store last time led was turned on
-
-const short intervals_msg[]={1000, 2000, 3000 , 5000};
-
-// constants won't change:
-short interval_msg = 3;           // interval at which to send messages (milliseconds)
-const short interval_led = 1000;
+const short intervals_msg[]={1000, 2000, 3000 , 5000};	// possible intervals to send messages
+short interval_msg = 3;					// interval index
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+	Serial.begin(BAUD_RATE);
+	while (!Serial);
 
-pinMode(BUTTON_PIN, INPUT);
-FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+	pinMode(BUTTON_PIN, INPUT);	// input switch
+	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 
-// Attach an interrupt to the ISR vector
-attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), button_ISR, FALLING);
+	// Attach an interrupt to the ISR vector
+	attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), button_ISR, FALLING);
 
 #ifdef ESP8266
-  LoRa.setPins(16, 17, 15); // set CS, reset, IRQ pin
+	LoRa.setPins(16, 17, 15); // set CS, reset, IRQ pin
 #endif
 
-  Serial.println("LoRa Sender");
-
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+	Serial.println("LoRa Sender");
+	if (!LoRa.begin(868E6)) {
+		Serial.println("Starting LoRa failed!");
+		while (1);
+	}
 }
 
-void button_ISR() {
-  interval_msg+=1;
-  if (interval_msg>3) { interval_msg=0; }
+void button_ISR() {	// called on button interruption
+	interval_msg+=1;
+	if (interval_msg>3) { interval_msg=0; }
 }
 
 void loop() {
 
-  unsigned long currentMillis = millis();
+	unsigned long currentMillis = millis();
 
-if (currentMillis - previousMillis >= intervals_msg[interval_msg]) {
-   // save the last time you blinked the LED
-   previousMillis = currentMillis;
+	if (currentMillis - previousMillis >= intervals_msg[interval_msg]) {
+		// save the last time we sent a message
+		previousMillis = currentMillis;
 
-   Serial.print("Sending packet: ");
-   Serial.println(counter);
+		Serial.print("Sending packet: ");
+		Serial.println(counter);
 
-  leds[0] = CRGB::Blue;
-  //leds[0] = CRGB::Red;
-  FastLED.show();
+		leds[0] = CRGB::Blue;
+		FastLED.show();
   
-  previousMillis_led=currentMillis;
+		// save the last time the led was turned on
+		previousMillis_led=previousMillis;
 
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
+		// send packet
+		LoRa.beginPacket();
+		LoRa.print("hello ");
+		LoRa.print(counter);
+		LoRa.endPacket();
 
-  counter++;
- }
+		counter++;
+	}
 
-// turn off after delay
-if (currentMillis - previousMillis_led >= interval_led) {
-  leds[0] = CRGB::Black;
-  FastLED.show();
-}
+	// turn off led after desired duration
+	if (currentMillis - previousMillis_led >= led_duration) {
+		leds[0] = CRGB::Black;
+		FastLED.show();
+	}
   
 }
